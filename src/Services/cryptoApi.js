b4f8aad95c8279bd4569eea35208467e3538fae8
Surya Sarkar <suryasarkar2@gmail.com>
2022-03-26 01:01:17 +0530

    First commit

---
+++ src/Services/cryptoApi.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "e816647ce8msh3218424219c3d49p175fbdjsn5348f3d31311",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     tiers: "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {},
+// };
